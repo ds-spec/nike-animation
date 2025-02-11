2a311d7c4643611bc4b0d08f6c0fab8fe2412ce6
Deepak <ds262969@gmail.com>
2025-02-11 22:11:03 +0530

    Completed the circular animation

--- src/components/screeen-loader/Loader.jsx
+++ src/components/screeen-loader/Loader.jsx
@@ -1,6 +1,6 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 
 const Loader = () => {
   const circularRef = useRef(null);
@@ -9,27 +9,52 @@ const Loader = () => {
       opacity: 0,
       duration: 2,
       ease: "circ.inOut",
-      width: "350px",
-      height: "350px",
-      border: "40px solid #e3e2e2",
+      scale: 2,
+      border: "40px solid #505050",
       transformOrigin: "center",
       // yoyo: true,
       repeat: -1,
     });
   }, []);
 
+  useEffect(() => {
+    var tl = gsap.timeline();
+
+    tl.to(".z-line-top", {
+      width: "450px",
+      duration: 0.5,
+      ease: "power1.inOut",
+    })
+      .to(".z-line-diagonal", {
+        strokeDashoffset: 0,
+        duration: 0.8,
+        ease: "power1.inOut",
+      })
+      .to(".z-line-bottom", {
+        strokeDashoffset: 0,
+        duration: 0.5,
+        ease: "power1.inOut",
+      });
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-between h-screen p-2 overflow-hidden">
-      <h1 className="uppercase font-general">innovation never stops</h1>
-      <div>
+    <div className="flex flex-col w-full items-center justify-between h-screen p-5 overflow-hidden">
+      <h1 className="uppercase font-general text-lg tracking-wider">
+        innovation never stops
+      </h1>
+      {/* <div className=""> */}
+      <div className="flex items-center flex-col gap-24">
         <div
           ref={circularRef}
-          className="w-[250px] h-[250px] z-20 rounded-full"
-        >
-          {/* <img src="/img/nike-navbar.png" alt="" className="size-56" /> */}
-        </div>
+          className="w-56 h-56 z-20 rounded-full border-[#505050] border-0 p-4 flex justify-center items-center"
+        ></div>
+        <h1 className="font-robert-medium text-3xl tracking-[1vw]">
+          JUST DO IT
+        </h1>
       </div>
-      <h1 className="uppercase font-general">breaking limits</h1>
+      <h1 className="uppercase font-general text-lg tracking-wider">
+        breaking limits
+      </h1>
     </div>
   );
 };
